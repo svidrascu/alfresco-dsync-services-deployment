352f5aac846b68d1b341a70e957fcb0dad3bd3f7
Marco Mancuso <marco.mancuso@alfresco.com>
2017-11-22 10:55:11 +0000

    dowloading the artifacts at build time

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,18 @@ FROM quay.io/alfresco/alfresco-base-java:0.1
 
 WORKDIR /usr/local/desktop-sync
 
+ENV LOCAL_ROOT=/usr/local/desktop-sync
+ARG ALFSS_URL=https://nightlybuilds.alfresco.com/DesktopSync/LATEST/5.x-Distribution-Zip/AlfrescoSyncServer.zip
+ARG POSTGRES_JDBC_VERSION=9.4.1212.jre6
 RUN yum -y install unzip
-RUN mkdir -p /usr/local/desktop-sync
+
+RUN mkdir -p $LOCAL_ROOT && \
+    curl --fail --retry 5 $ALFSS_URL -o $LOCAL_ROOT/AlfrescoSyncServer.zip && \
+    unzip AlfrescoSyncServer.zip -d $LOCAL_ROOT && \
+    curl -L -o $LOCAL_ROOT/sync/service-sync/postgresljdbc.jar http://jdbc.postgresql.org/download/postgresql-$POSTGRES_JDBC_VERSION.jar
+
 COPY sync-start.sh /usr/local/desktop-sync
 COPY config.yml /usr/local/desktop-sync
 RUN chmod +x /usr/local/desktop-sync/sync-start.sh
 
-ENTRYPOINT ["/usr/local/desktop-sync/sync-start.sh", "true"]
+ENTRYPOINT ["/usr/local/desktop-sync/sync-start.sh"]
--- sync-start.sh
+++ sync-start.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 set -x
       ### Fill in these bits:
-      DESKTOPSYNC_URL=https://nightlybuilds.alfresco.com/DesktopSync/LATEST/5.x-Distribution-Zip/AlfrescoSyncServer.zip
-      POSTGRES_JDBC_URL=https://jdbc.postgresql.org/download/postgresql-9.4.1212.jre6.jar
-      LOCAL_ROOT=/usr/local/desktop-sync
-       mkdir -p $LOCAL_ROOT
+      LOCAL_ROOT="${LOCAL_ROOT:-/usr/local/desktop-sync}"
       JAR_LOCATION=$LOCAL_ROOT/sync/service-sync
-      SYNC_JAR_VERSION=2.2-SNAPSHOT
-      SYNC_JAR_FILE=service-sync-$SYNC_JAR_VERSION.jar
-      DB_CONNECTOR_JAR_FILE=$JAR_LOCATION/postgresql.jar
+      SYNC_JAR_FILE="service-sync-*.jar"
       SYNC_TMP_DIR=$JAR_LOCATION
-      MAX_JAVA_HEAP_SIZE=2G
+      MAX_JAVA_HEAP_SIZE="${MAX_JAVA_HEAP_SIZE:-2G}"
       NAME="alfresco-syncservice"
       PID_FILE=$JAR_LOCATION/syncservice.pid
       ### Start of JMX config ###
       ### true | false
-      ENABLE_JMX_REMOTE=false
-      JMX_REMOTE_PORT=50800
-      JMX_RMI_HOSTNAME=localhost
-      JMX_REMOTE_RMI_PORT=50801
+      ENABLE_JMX_REMOTE="${ENABLE_JMX_REMOTE:-false}"
+      JMX_REMOTE_PORT="${JMX_REMOTE_PORT:-0800}"
+      JMX_RMI_HOSTNAME="${JMX_RMI_HOSTNAME:-localhost}"
+      JMX_REMOTE_RMI_PORT="${JMX_REMOTE_RMI_PORT:-50801}"
       ### true | false
-      ENABLE_JMX_REMOTE_AUTHENTICATION=true
+      ENABLE_JMX_REMOTE_AUTHENTICATION="${ENABLE_JMX_REMOTE_AUTHENTICATION:-true}"
       ### If you enable JMX_REMOTE_AUTHENTICATION, then, set the next two properties (ie.PASSWORD_FILE, ACCESS_FILE)
       ### Make sure to restict (e.g. sudo chmode 600 jmx.password) the JMX_PASSWORD_FILE to the specified USER.
       JMX_PASSWORD_FILE=$JAR_LOCATION/jmx/jmx.password.properties
@@ -28,57 +23,43 @@ set -x
       ### true | false
       ### Caution - if you set 'ENABLE_JMX_REMOTE_AUTHENTICATION' and 'ENABLE_JMX_REMOTE_SSL' to false, any remote user who knows
       ### (or guesses) your port number and host name will be able to monitor and control your applications and platform.
-      ENABLE_JMX_REMOTE_SSL=true
+      # ENABLE_JMX_REMOTE_SSL="${ENABLE_JMX_REMOTE_SSL:-true}"
       ### If you enable JMX_REMOTE_AUTHENTICATION, then, set the next six properties
       ### Make sure to restict (e.g. sudo chmode 600 jmx.password) the JMX_PASSWORD_FILE to the specified USER.
       ### Keystore details
-      JMX_KEYSTORE=/$JAR_LOCATION/sync.keystore
-      JMX_KEYSTORE_PASSWORD=alfresco
-      JMX_KEYSTORE_TYPE=JCEKS
+      # JMX_KEYSTORE=/$JAR_LOCATION/sync.keystore
+      # JMX_KEYSTORE_PASSWORD="${JMX_KEYSTORE_PASSWORD:-alfresco}"
+      # JMX_KEYSTORE_TYPE="${JMX_KEYSTORE_TYPE:-JCEKS}"
       ### Truststore details
-      JMX_TRUSTSTORE=$JAR_LOCATION/sync.truststore
-      JMX_TRUSTSTORE_PASSWORD=alfresco
-      JMX_TRUSTSTORE_TYPE=JCEKS
+      # JMX_TRUSTSTORE=$JAR_LOCATION/sync.truststore
+      # JMX_TRUSTSTORE_PASSWORD="${JMX_TRUSTSTORE_PASSWORD:-alfresco}"
+      # JMX_TRUSTSTORE_TYPE=JCEKS
       ### End of JMX config ###
+      DB_HOST="${DB_HOST:-localhost}"
+      OVERRIDE_CONFIG="${OVERRIDE_CONFIG:-true}"
 
-      ### downloading sync distribution zip
-      if [ -f AlfrescoSyncServer.zip ]; then
-        echo "found: sync-dist-5.x-1.1.zip"
-      else
-        echo "downloading: sync-dist-5.x-1.1.zip ... $desksync_url"
-        curl --fail --retry 5 $DESKTOPSYNC_URL -o AlfrescoSyncServer.zip
-      fi
-      if [ ! -d sync-dist ]; then
-            unzip AlfrescoSyncServer.zip -d $LOCAL_ROOT
-            echo $LOCAL_ROOT
-      fi
-      if [ -f $JAR_LOCATION/postgres.jar ]; then
-        echo "found: Postgres JDBC driver"
-      else
-        echo "downloading: Postgres JDBC driver ... $postgres_jdbc_url"
-        curl --fail --retry 5 $POSTGRES_JDBC_URL -o $JAR_LOCATION/postgresql.jar
-      fi
+      sed -i "s~@@HOST@@~$DB_HOST~g" $LOCAL_ROOT/config.yml
 
-      if [ "$1" == "true" ]; then
-        echo "IS IT $1 "
-           cp -rf $LOCAL_ROOT/config.yml $JAR_LOCATION
+      if [ "${OVERRIDE_CONFIG}" == "true" ]; then
+          cp -rf $LOCAL_ROOT/config.yml $JAR_LOCATION
       fi
 
       JMX_CONF="-Dcom.sun.management.jmxremote=$ENABLE_JMX_REMOTE"
-      JMX_SECURITY_CONF=""
-      if [ $ENABLE_JMX_REMOTE = true ]; then
-      	JMX_CONF="$JMX_CONF -Djava.rmi.server.hostname=$JMX_RMI_HOSTNAME -Dcom.sun.management.jmxremote.port=$JMX_REMOTE_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_REMOTE_RMI_PORT -Dcom.sun.management.jmxremote.authenticate=$ENABLE_JMX_REMOTE_AUTHENTICATION -Dcom.sun.management.jmxremote.ssl=$ENABLE_JMX_REMOTE_SSL"
-      	if [ $ENABLE_JMX_REMOTE_AUTHENTICATION = true ]; then
-      		JMX_SECURITY_CONF="-Dcom.sun.management.jmxremote.access.file=$JMX_ACCESS_FILE -Dcom.sun.management.jmxremote.password.file=$JMX_PASSWORD_FILE"
-      	fi
-      	if [ $ENABLE_JMX_REMOTE_SSL = true ]; then
-      		JMX_SECURITY_CONF="$JMX_SECURITY_CONF -Djavax.net.ssl.keyStore=$JMX_KEYSTORE -Djavax.net.ssl.keyStorePassword=$JMX_KEYSTORE_PASSWORD -Djavax.net.ssl.keyStoreType=$JMX_KEYSTORE_TYPE -Djavax.net.ssl.trustStore=$JMX_TRUSTSTORE -Djavax.net.ssl.trustStoreType=$JMX_TRUSTSTORE_TYPE -Djavax.net.ssl.trustStorePassword=$JMX_TRUSTSTORE_PASSWORD"
-      	fi
-      	JMX_CONF="$JMX_CONF $JMX_SECURITY_CONF"
-      fi
+      # JMX_SECURITY_CONF=""
+      # if [ $ENABLE_JMX_REMOTE = true ]; then
+      # 	JMX_CONF="$JMX_CONF -Djava.rmi.server.hostname=$JMX_RMI_HOSTNAME -Dcom.sun.management.jmxremote.port=$JMX_REMOTE_PORT -Dcom.sun.management.jmxremote.rmi.port=$JMX_REMOTE_RMI_PORT -Dcom.sun.management.jmxremote.authenticate=$ENABLE_JMX_REMOTE_AUTHENTICATION -Dcom.sun.management.jmxremote.ssl=$ENABLE_JMX_REMOTE_SSL"
+      # 	if [ $ENABLE_JMX_REMOTE_AUTHENTICATION = true ]; then
+      # 		JMX_SECURITY_CONF="-Dcom.sun.management.jmxremote.access.file=$JMX_ACCESS_FILE -Dcom.sun.management.jmxremote.password.file=$JMX_PASSWORD_FILE"
+      # 	fi
+      # 	if [ $ENABLE_JMX_REMOTE_SSL = true ]; then
+      # 		JMX_SECURITY_CONF="$JMX_SECURITY_CONF -Djavax.net.ssl.keyStore=$JMX_KEYSTORE -Djavax.net.ssl.keyStorePassword=$JMX_KEYSTORE_PASSWORD -Djavax.net.ssl.keyStoreType=$JMX_KEYSTORE_TYPE -Djavax.net.ssl.trustStore=$JMX_TRUSTSTORE -Djavax.net.ssl.trustStoreType=$JMX_TRUSTSTORE_TYPE -Djavax.net.ssl.trustStorePassword=$JMX_TRUSTSTORE_PASSWORD"
+      # 	fi
+      # 	JMX_CONF="$JMX_CONF $JMX_SECURITY_CONF"
+      # fi
 
       PGREP_STRING="$SYNC_JAR_FILE"
-      START_CMD="\"cd $JAR_LOCATION;java -Xmx$MAX_JAVA_HEAP_SIZE $JMX_CONF -Djava.io.tmpdir=$SYNC_TMP_DIR -cp $DB_CONNECTOR_JAR_FILE:$SYNC_JAR_FILE org.alfresco.service.sync.dropwizard.SyncService server config.yml > $JAR_LOCATION/sync.log 2>&1 &\""
+      JARS=$(find $JAR_LOCATION -name "*.jar" | paste -sd ":" -)
+      START_CMD="\"cd $JAR_LOCATION;java -Xmx$MAX_JAVA_HEAP_SIZE $JMX_CONF -Djava.io.tmpdir=$SYNC_TMP_DIR -cp $JARS org.alfresco.service.sync.dropwizard.SyncService server config.yml > $JAR_LOCATION/sync.log 2>&1 &\""
 
       log_success_msg() {
       	echo "$*"
